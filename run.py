5b0afa33478eadec88dd6199ac4c254a1f238693
Deniz Pandar <denizpandar@gmail.com>
2025-08-08 14:46:56 +0100

    Add code to read mood history JSON file with proper encoding

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from colorama import init, Fore, Back, Style
-import os   # Operating System - 
+import os   # Operating System
 import json
 
 # Start Colorama
@@ -15,3 +15,8 @@ moods = {
 
 # Filename of the JSON file used to read and write mood data
 MOOD_FILE = 'mood-history.json'
+
+# Reading the file
+# 'r' is for reading the file, encoding is for special caracters on json file
+with open('mood-history.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
