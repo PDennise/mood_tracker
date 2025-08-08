2a0951ad3f19b67622ba029da2cf1e4555a82c9a
Deniz Pandar <denizpandar@gmail.com>
2025-08-08 14:16:27 +0100

    Style moods and background

--- run.py
+++ run.py
@@ -2,3 +2,11 @@ from colorama import init, Fore, Back, Style
 
 # Start Colorama
 init(autoreset=True)
+
+moods = { 
+    "happy": (Back.WHITE, Fore.GREEN),
+    "sad": (Back.BLUE, Fore.BLACK),
+    "angry": (Back.BLACK, Fore.RED),
+    "calm": (Back.GREEN, Fore.WHITE),
+    "anxius": (Back.YELLOW, Fore.LIGHTRED_EX)
+}
