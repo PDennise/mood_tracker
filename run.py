d8100eee92e26d96d0f09ab0f61f6c3ec7e11c6a
Deniz Pandar <denizpandar@gmail.com>
2025-08-08 14:34:33 +0100

    Add imports and define mood history JSON file path with comments

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
 from colorama import init, Fore, Back, Style
+import os   # Operating System - 
+import json
 
 # Start Colorama
 init(autoreset=True)
 
-moods = { 
+moods = {
     "happy": (Back.WHITE, Fore.GREEN),
     "sad": (Back.BLUE, Fore.BLACK),
     "angry": (Back.BLACK, Fore.RED),
     "calm": (Back.GREEN, Fore.WHITE),
     "anxius": (Back.YELLOW, Fore.LIGHTRED_EX)
 }
+
+# Filename of the JSON file used to read and write mood data
+MOOD_FILE = 'mood-history.json'
